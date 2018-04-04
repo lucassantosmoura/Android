8fe44b3c34182afb4f0c295dcc19c4ea70007a90
logonrm <logonrm@fiap.com.br>
2018-04-04 10:26:55 -0300

    Android Layouts

---
+++ app/src/main/java/br/com/fiap/layouts/ExemploTableActivity.java
@@ -0,0 +1,14 @@
+
+package br.com.fiap.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExemploTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exemplo_table);
+    }
+}
